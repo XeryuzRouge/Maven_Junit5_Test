af955701afa98e7251653d0a3eb43861156c794e
Germán <alienslocos2000_@hotmail.com>
2018-09-27 16:12:49 -0500

    git reset --hard HEAD^

--- src/main/java/com/mycompany/app/FizzBuzz.java
+++ src/main/java/com/mycompany/app/FizzBuzz.java
@@ -7,7 +7,7 @@ public class FizzBuzz {
 
   public void one_to_hundred() {
 
-  for(int i=0; i<=100; i++){
+    for(int i=0; i<=100; i++){
     String j = "";
       if(i % 3 != 0 && i % 5 != 0){
         j = Integer.toString(i);
@@ -20,8 +20,11 @@ public class FizzBuzz {
           j += "Buzz";
         }
       }
-              System.out.print(j + ", ");
-         }
+              System.out.print(j);
+              if(i<100){
+              System.out.print(", ");
+          }
+    }
   }
 
 }
