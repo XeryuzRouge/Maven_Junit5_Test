7a95712d4b02c45708e0bae0caef483bc4b4d55e
Germán <alienslocos2000_@hotmail.com>
2018-09-28 11:24:39 -0500

    The java style guide has been applied to the code

--- src/main/java/com/mycompany/app/FizzBuzz.java
+++ src/main/java/com/mycompany/app/FizzBuzz.java
@@ -5,23 +5,23 @@ import java.io.*;
 
 public class FizzBuzz {
 
-  public void one_to_hundred() {
+  public void oneToHundred() {
 
-    for(int i=0; i<=100; i++){
+    for(int i=1; i<=100; i++) {
     String j = "";
-      if(i % 3 != 0 && i % 5 != 0){
+      if (i % 3 != 0 && i % 5 != 0) {
         j = Integer.toString(i);
       }
-      else{
-        if(i % 3 == 0){
-          j += "fizz";
+      else {
+        if (i % 3 == 0) {
+          j += "Fizz";
         }
-        if(i % 5 == 0){
+        if (i % 5 == 0) {
           j += "Buzz";
         }
       }
       System.out.print(j);
-      if(i<100){
+      if (i<100){
         System.out.print(", ");
       }
     }
--- src/test/java/com/mycompany/app/FizzBuzzTests.java
+++ src/test/java/com/mycompany/app/FizzBuzzTests.java
@@ -14,33 +14,29 @@ class FizzBuzzTests {
   private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
   private final PrintStream originalOut = System.out;
 
-  @BeforeEach
-  public void setUpStreams() {
+  @BeforeEach public void setUpStreams() {
     System.setOut(new PrintStream(outContent));
   }
 
-  @AfterEach
-  public void restoreStreams() {
+  @AfterEach public void restoreStreams() {
     System.setOut(originalOut);
   }
 
-  @Test
-  public void one_to_hundred() {
-    FizzBuzz fizzbuzz = new FizzBuzz();
-    fizzbuzz.one_to_hundred();
-    String expected_list = "";
+  @Test public void oneToHundred_oneToHundred() {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    fizzBuzz.oneToHundred();
 
-    expected_list = "fizzBuzz, 1, 2, fizz, 4, Buzz, fizz, 7, 8, fizz, Buzz, " +
-    "11, fizz, 13, 14, fizzBuzz, 16, 17, fizz, 19, Buzz, " +
-    "fizz, 22, 23, fizz, Buzz, 26, fizz, 28, 29, fizzBuzz, " +
-    "31, 32, fizz, 34, Buzz, fizz, 37, 38, fizz, Buzz, " +
-    "41, fizz, 43, 44, fizzBuzz, 46, 47, fizz, 49, Buzz, " +
-    "fizz, 52, 53, fizz, Buzz, 56, fizz, 58, 59, fizzBuzz, " +
-    "61, 62, fizz, 64, Buzz, fizz, 67, 68, fizz, Buzz, " +
-    "71, fizz, 73, 74, fizzBuzz, 76, 77, fizz, 79, Buzz, " +
-    "fizz, 82, 83, fizz, Buzz, 86, fizz, 88, 89, fizzBuzz, " +
-    "91, 92, fizz, 94, Buzz, fizz, 97, 98, fizz, Buzz";
+    String expectedList = "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, "
+        + "11, Fizz, 13, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz, "
+            + "Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz, "
+                + "31, 32, Fizz, 34, Buzz, Fizz, 37, 38, Fizz, Buzz, "
+                    + "41, Fizz, 43, 44, FizzBuzz, 46, 47, Fizz, 49, Buzz, "
+                        + "Fizz, 52, 53, Fizz, Buzz, 56, Fizz, 58, 59, FizzBuzz, "
+                            + "61, 62, Fizz, 64, Buzz, Fizz, 67, 68, Fizz, Buzz, "
+                                + "71, Fizz, 73, 74, FizzBuzz, 76, 77, Fizz, 79, Buzz, "
+                                    + "Fizz, 82, 83, Fizz, Buzz, 86, Fizz, 88, 89, FizzBuzz, "
+                                        + "91, 92, Fizz, 94, Buzz, Fizz, 97, 98, Fizz, Buzz";
 
-    assertEquals(expected_list, outContent.toString());
+    assertEquals(expectedList, outContent.toString());
   }
 }
