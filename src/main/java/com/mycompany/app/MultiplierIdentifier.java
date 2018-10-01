a776e79e00e569b6529d7a9d00802b900aa69882
Germán <alienslocos2000_@hotmail.com>
2018-10-01 11:04:40 -0500

    MutliplierIdentifier created

---
+++ src/main/java/com/mycompany/app/MultiplierIdentifier.java
@@ -0,0 +1,21 @@
+package com.mycompany.app;
+
+import java.io.*;
+import java.util.*;
+
+public class MultiplierIdentifier {
+
+  public int[] getMultipliersOf(int multiplier, int[] listToCheck) {
+    List<Integer> listToReturn = new ArrayList<Integer>();
+    for(int i = 0;i < listToCheck.length; i++) {
+      if(listToCheck[i] % multiplier == 0) {
+        listToReturn.add(listToCheck[i]);
+      }
+    }
+
+    return listToReturn.stream()
+                        .mapToInt(Integer::intValue)
+                        .toArray();
+  }
+  
+}
