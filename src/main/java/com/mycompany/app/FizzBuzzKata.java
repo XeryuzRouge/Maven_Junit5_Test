db7951d83976d33e1b71ba7ceb820b470689d1ef
Germán <alienslocos2000_@hotmail.com>
2018-10-01 14:08:51 -0500

    'FizzBuzzKata' added and the final test was implemented

---
+++ src/main/java/com/mycompany/app/FizzBuzzKata.java
@@ -0,0 +1,29 @@
+package com.mycompany.app;
+
+import java.io.*;
+import java.util.*;
+
+public class FizzBuzzKata {
+
+  public void oneToHundred(){
+    ListGenerator listGenerator = new ListGenerator();
+    int[] generatedList = listGenerator.generate(100);
+
+    MultiplierIdentifier multiplierIdentifier = new MultiplierIdentifier();
+    int[] multipliersOf3 = multiplierIdentifier.getMultipliersOf(3, generatedList);
+    int[] multipliersOf5 = multiplierIdentifier.getMultipliersOf(5, generatedList);
+
+    ValuesExchanger valuesExchanger = new ValuesExchanger();
+    String[] fizzList = valuesExchanger.numbersToStrings("Fizz", multipliersOf3, generatedList);
+    String[] buzzList = valuesExchanger.numbersToStrings("Buzz", multipliersOf5, generatedList);
+
+    ElementsZipper elementsZipper = new ElementsZipper();
+    String[] priorityValues = {"Fizz", "Buzz"};
+    String[] results = elementsZipper.zipThese(fizzList, buzzList, priorityValues);
+
+    Printer printer = new Printer();
+    String resultsAsString = Arrays.toString(results);
+    printer.show(resultsAsString);
+  }
+
+}
--- src/test/java/com/mycompany/app/PrinterTests.java
+++ src/test/java/com/mycompany/app/PrinterTests.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.*;
 import java.io.*;
 
@@ -60,13 +59,30 @@ class FizzBuzzTests {
     assertArrayEquals(expectedValues, results);
   }
 
-  @Test public void ArraysZipper_getArrayZipped() {
-    ArraysZipper arraysZipper = new ArraysZipper();
-    String[] firstList = {"Fizz", "13", "14", "Fizz", "16", "17", "Fizz"};
-    String[] secondList = {"12", "13", "14", "Buzz", "16", "17", "18"};
+  @Test public void ElementsZipper_getArrayZipped() {
+    ElementsZipper elementsZipper = new ElementsZipper();
+    String[] firstList = {"Fizz", "13", "14", "Fizz", "16", "17", "Fizz", "19", "20"};
+    String[] secondList = {"12", "13", "14", "Buzz", "16", "17", "18", "19", "Buzz"};
     String[] priorityValues = {"Fizz", "Buzz"};
-    String[] results = arraysZipper.zipThese(firstList, secondList, priorityValues);
-    String[] expectedList = {"Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz"};
+    String[] results = elementsZipper.zipThese(firstList, secondList, priorityValues);
+    String[] expectedList = {"Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz"};
     assertArrayEquals(expectedList, results);
+  }
+
+  @Test public void printFinalList() {
+    FizzBuzzKata fizzBuzzKata = new FizzBuzzKata();
+    fizzBuzzKata.oneToHundred();
+
+    String expectToPrint = "[1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, " + 
+        "11, Fizz, 13, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz, " +
+            "Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz, " +
+                "31, 32, Fizz, 34, Buzz, Fizz, 37, 38, Fizz, Buzz, " +
+                    "41, Fizz, 43, 44, FizzBuzz, 46, 47, Fizz, 49, Buzz, " +
+                        "Fizz, 52, 53, Fizz, Buzz, 56, Fizz, 58, 59, FizzBuzz, " +
+                            "61, 62, Fizz, 64, Buzz, Fizz, 67, 68, Fizz, Buzz, " +
+                                "71, Fizz, 73, 74, FizzBuzz, 76, 77, Fizz, 79, Buzz, " +
+                                    "Fizz, 82, 83, Fizz, Buzz, 86, Fizz, 88, 89, FizzBuzz, " +
+                                        "91, 92, Fizz, 94, Buzz, Fizz, 97, 98, Fizz, Buzz]";
+    assertEquals(expectToPrint, outContent.toString());
   }
 }
