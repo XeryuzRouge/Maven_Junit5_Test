62193635389bf73143a67efe2f4661d2934e964d
Germán <alienslocos2000_@hotmail.com>
2018-10-01 11:01:41 -0500

    ListGenerator & MultiplierIdentifier's tests ready

--- src/test/java/com/mycompany/app/PrinterTests.java
+++ src/test/java/com/mycompany/app/PrinterTests.java
@@ -2,10 +2,14 @@
 package com.mycompany.app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.*;
 import java.io.*;
 
 class FizzBuzzTests {
@@ -28,6 +32,23 @@ class FizzBuzzTests {
     printer.show(expectToPrint);
 
     assertEquals(expectToPrint, outContent.toString());
+  }
+
+  @Test public void ListGenerator_getWhatItGenerates() {
+    ListGenerator listGenerator = new ListGenerator();
+    int[] expectedList = {1, 2, 3};
+    int[] generatedList = listGenerator.generate(3);
+    assertArrayEquals(expectedList, generatedList);
+  }
+
+  @Test public void MultiplierIdentifier_getMultipliers() {
+    MultiplierIdentifier multiplierIdentifier = new MultiplierIdentifier();
+    int[] exampleList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int multipliersOf = 3;
+    int[] results = multiplierIdentifier.getMultipliersOf(multipliersOf, exampleList);
+
+    int[] expectedNumbers = {3, 6, 9, 12};
+    assertArrayEquals(expectedNumbers, results);
   }
 
 }
