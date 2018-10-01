555706d2ad0e39ebfa41559efca93bb0f6453cb4
Germán <alienslocos2000_@hotmail.com>
2018-10-01 14:06:38 -0500

    'ArraysZipper' renamed to 'ElementsZipper'

---
+++ src/main/java/com/mycompany/app/ElementsZipper.java
@@ -0,0 +1,33 @@
+package com.mycompany.app;
+
+import java.io.*;
+import java.util.*;
+
+public class ElementsZipper {
+
+   public String[] zipThese(String[] firstList, String[] secondList, String[] priorityValues) {
+    List<String> zippedList = new ArrayList<String>();
+    for(int i = 0;i < firstList.length; i++) {
+      String newElement = "";
+      if (!firstList[i].equals(secondList[i])) {
+        for (int j = 0;j < priorityValues.length; j++) {
+          if (firstList[i].equals(priorityValues[j])) {
+            newElement = newElement + firstList[i];
+          }
+          if (secondList[i].equals(priorityValues[j])) {
+            newElement = newElement + secondList[i];
+          }
+        }
+      }
+      else  {
+        newElement = firstList[i];
+      }
+      zippedList.add(newElement);
+    }
+    String[] arrayToReturn = new String[zippedList.size()];
+    arrayToReturn = zippedList.toArray(arrayToReturn); 
+
+    return arrayToReturn;
+  }    
+  
+}
